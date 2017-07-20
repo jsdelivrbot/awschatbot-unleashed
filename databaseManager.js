a4c47df1f0099db4ff4529ff3d92c94e62f1253e
Vikas Bajaj <vikasbajajs@gmail.com>
2017-07-20 22:47:50 +0530

    Update databaseManager.js

--- databaseManager.js
+++ databaseManager.js
@@ -39,7 +39,7 @@ module.exports.createCarBid = function(userId,
 	let auctionCreateDate = date.format(now,'YYYY-MM-DD');
 	let tempAuctionExpiryDate = date.addDays(now,numberofDays);
 	let auctionExpiryDate = date.format(tempAuctionExpiryDate,'YYYY-MM-DD');
-	
+
 	console.log(`Auction Creation Date is ${auctionCreateDate}`);
 	console.log(`Auction Expiry Date is ${auctionExpiryDate}`);
 	item.auction_create_date = auctionCreateDate;
@@ -49,7 +49,7 @@ module.exports.createCarBid = function(userId,
 
 	//item: it is a new bid record just created in saveItemToTable
 	return saveItemToTable('car-bid-master', item).then((item)=>{
-		var table = "dealer-market-place-tokens";
+				var table = "dealer-market-place-tokens";
         var marketPlaceType = "CarDealers";
         var params = {
 	        		TableName: table,
@@ -70,55 +70,55 @@ module.exports.createCarBid = function(userId,
 	});
 };
 module.exports.findBids = function(bidRef) {
-	const params = {
-	    TableName: 'car-bid-details',
-	    KeyConditionExpression: "bid_reference = :a",
-	    ExpressionAttributeValues : {
-	      ":a":bidRef
-	    }
-  	};
-	const getAsync = promisify(dynamo.query, dynamo);
-	return getAsync(params).then(response => {
-	    if (_.isEmpty(response)) {
-	      console.log(`Bid with bidId:${bidRef} not found`);
-	      return Promise.reject(new Error(`Bid with bidId:${bidRef} not found`));
-	    }
-    	console.log(response);
-    	return response;
-  	});
+		const params = {
+		    TableName: 'car-bid-details',
+		    KeyConditionExpression: "bid_reference = :a",
+		    ExpressionAttributeValues : {
+		      ":a":bidRef
+		    }
+	  	};
+		const getAsync = promisify(dynamo.query, dynamo);
+		return getAsync(params).then(response => {
+		    if (_.isEmpty(response)) {
+		      console.log(`Bid with bidId:${bidRef} not found`);
+		      return Promise.reject(new Error(`Bid with bidId:${bidRef} not found`));
+		    }
+	    	console.log(response);
+	    	return response;
+	  	});
 };
 module.exports.recordBidSubmission = function(bidReference,dealerName,dealerSlackId,bidAmount) {
-	const item = {};
-	item.bid_reference = bidReference;
-	item.dealer_reference = dealerSlackId;
-	item.dealer_name = dealerName;
-	item.bid_amount = bidAmount;
-	//item: it is a new bid record just created in saveItemToTable
-	return saveItemToTable('car-bid-details', item).then((item)=>{
-		return Promise.resolve(item);
-	});
+		const item = {};
+		item.bid_reference = bidReference;
+		item.dealer_reference = dealerSlackId;
+		item.dealer_name = dealerName;
+		item.bid_amount = bidAmount;
+		//item: it is a new bid record just created in saveItemToTable
+		return saveItemToTable('car-bid-details', item).then((item)=>{
+			return Promise.resolve(item);
+		});
 };
 module.exports.checkImageUpload = function(uniqueReferenceNumber) {
-	const params = {
-	    TableName: 'bid-master-images',
-	    KeyConditionExpression: "bid_ref = :a",
-	    ExpressionAttributeValues : {
-	      ":a":uniqueReferenceNumber
-	    }
-  	};
-	const getAsync = promisify(dynamo.query, dynamo);
-	return getAsync(params).then(response => {
-	    if (_.isEmpty(response)) {
-	      console.log(`================No images found in the database uniqueReferenceNumber ${uniqueReferenceNumber}`);
-	      return Promise.reject(new Error(`No images found in the database uniqueReferenceNumber ${uniqueReferenceNumber}`));
-	    }
-    	console.log(`Found image record for refernce Number : ${response}`);
-    	return response;
-  	});
+		const params = {
+		    TableName: 'bid-master-images',
+		    KeyConditionExpression: "bid_ref = :a",
+		    ExpressionAttributeValues : {
+		      ":a":uniqueReferenceNumber
+		    }
+	  	};
+		const getAsync = promisify(dynamo.query, dynamo);
+		return getAsync(params).then(response => {
+		    if (_.isEmpty(response)) {
+		      console.log(`================No images found in the database uniqueReferenceNumber ${uniqueReferenceNumber}`);
+		      return Promise.reject(new Error(`No images found in the database uniqueReferenceNumber ${uniqueReferenceNumber}`));
+		    }
+	    	console.log(`Found image record for refernce Number : ${response}`);
+	    	return response;
+	  	});
 };
 module.exports.validateUserIdAndBidRef = function(bidRef,userId)
 {
-	console.log(`INSIDE validateUserId And Bid Reference number ${bidRef} and user id ${userId}`);	
+	console.log(`INSIDE validateUserId And Bid Reference number ${bidRef} and user id ${userId}`);
 	const params = {
 	    TableName: 'car-bid-master',
 	    Key:{
@@ -135,7 +135,7 @@ module.exports.validateUserIdAndBidRef = function(bidRef,userId)
 	    if(response.Item.userId !== userId)
 	    {
 	    	console.log(`Bid Reference ${bidRef} Does not belong to user ${userId}`);
-	      	return Promise.reject(new Error(`Bid Reference ${bidRef} does not belong to you hence please provide valid reference number`));	
+	      	return Promise.reject(new Error(`Bid Reference ${bidRef} does not belong to you hence please provide valid reference number`));
 	    }*/
     	console.log(response);
     	return Promise.resolve(response);
@@ -143,7 +143,7 @@ module.exports.validateUserIdAndBidRef = function(bidRef,userId)
 };
 module.exports.validateBidRefAndDealerRef = function(bidRef,dealerRef)
 {
-	console.log(`INSIDE validateBidRefAndDealerRef bid ref ${bidRef} and dealer ref ${dealerRef}`);	
+	console.log(`INSIDE validateBidRefAndDealerRef bid ref ${bidRef} and dealer ref ${dealerRef}`);
 	const params = {
 	    TableName: 'car-bid-details',
 	    KeyConditionExpression: "bid_reference = :a",
@@ -160,7 +160,7 @@ module.exports.validateBidRefAndDealerRef = function(bidRef,dealerRef)
 
 module.exports.checkValidityOfAuction = function(bidRef)
 {
-	console.log(`INSIDE checkValidityOfAuction And Bid Reference number ${bidRef}`);	
+	console.log(`INSIDE checkValidityOfAuction And Bid Reference number ${bidRef}`);
 	const params = {
 	    TableName: 'car-bid-master',
 	    Key:{
@@ -178,8 +178,56 @@ module.exports.checkValidityOfAuction = function(bidRef)
     	return response;
   	});
 };
-
-
+module.exports.checkValidBidReference = function(bidRef)
+{
+	console.log(`INSIDE checkValidBidReference And Bid Reference number ${bidRef}`);
+	const params = {
+			TableName: 'car-bid-master',
+			Key:{
+					'bid_reference': bidRef
+				}
+		};
+	const getAsync = promisify(dynamo.get, dynamo);
+	return getAsync(params).then(response => {
+		console.log(`After hitting dynamo.getItem response is ${JSON.stringify(response)}`);
+			if (_.isEmpty(response)) {
+				console.log(`Bid with bidId:${bidRef} not found`);
+				return Promise.resolve("N");
+			}
+			return Promise.resolve("Y");
+		});
+};
+module.exports.createChannelDetailsRecord = function(channelId,auctionExpiryDate,uniqueReferenceNumber)
+{
+		const item = {};
+		item.channel_id = channelId;
+		item.channel_expiry_date = auctionExpiryDate;
+		item.bid_reference = uniqueReferenceNumber;
+		//item: it is a new bid record just created in saveItemToTable
+		return saveItemToTable('channel-details', item).then((item)=>{
+				return Promise.resolve(item);
+		});
+};
+module.exports.getSlackTeamSecurityToken = function(){
+		var table = "dealer-market-place-tokens";
+		var marketPlaceType = "CarDealers";
+		var params = {
+					TableName: table,
+					KeyConditionExpression: "market_place_type = :a",
+				ExpressionAttributeValues : {
+						":a":marketPlaceType
+				}
+			}
+			const getAsync = promisify(dynamo.query, dynamo);
+			return getAsync(params).then(response => {
+				if (_.isEmpty(response)) {
+					console.log('Could not get the security token out of dealer-market place token table');
+					return Promise.reject(new Error('Could not get the security token out of dealer-market place token table'));
+				}
+				console.log(response);
+				return response;
+			});
+}
 function saveItemToTable(tableName, item) {
 	const params = {
     	TableName: tableName,
