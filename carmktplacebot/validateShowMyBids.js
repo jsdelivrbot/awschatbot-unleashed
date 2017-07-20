8aa1595b2c681f97a820f22d3efe501689784dd8
Vikas Bajaj <vikasbajajs@gmail.com>
2017-07-20 22:51:50 +0530

    Update validateShowMyBids.js

--- carmktplacebot/validateShowMyBids.js
+++ carmktplacebot/validateShowMyBids.js
@@ -5,36 +5,49 @@ var validator = require('validator');
 const promisify = require('es6-promisify');
 const databaseManager = require('../databaseManager');
 
-module.exports = function (bidRef,dealerRef,userId) {
-     
-     if(bidRef !== null && dealerRef === null)
+module.exports = function (bidRef,dealerRef,userId,interestedInBid) {
+
+     if(bidRef !== null && interestedInBid === null)
      {
+            console.log(`Inside Validate show my bids and validating User id and bidreference number ${bidRef} and dealer ref ${dealerRef}`);
             return databaseManager.validateUserIdAndBidRef(bidRef,userId).then(response => {
-            
+
                   if (_.isEmpty(response)) {
                         return Promise.resolve(buildValidationResult(false,
                                                                      'BidRef',
                                                                      `Could not find any bids which matches *${bidRef}* Please try again and make sure you have caps lock off :smile:`,
-                                                                     null,null,null));   
+                                                                     null,null,null));
                   }
                   if(response.Item.userId !== userId)
                   {
-                         return Promise.resolve(buildValidationResult(false,'BidRef',
-                                                                     `Bid Reference ${bidRef} does not belong to you hence please provide valid reference number`,
-                                                                     null,null,null));   
+                        console.log(`bid reference ${bidRef} does not belong to you.`);
+                        console.log(`recieved user id ${userId} and bid user id is ${response.Item.userId}`);
+                        return Promise.resolve(buildValidationResult(false,'BidRef',
+                                                                     `:flushed: Bid Reference ${bidRef} does not belong to you. Enter your bid reference and try again`,
+                                                                     null,null,null));
                   }
-                  return Promise.resolve(buildValidationResult(true, null,null,null,null,null));   
+                  return Promise.resolve(buildValidationResult(true, null,null,null,null,null));
             });
       }
-      
+      if(bidRef !== null && interestedInBid !== null && dealerRef === null)
+      {
+          if(interestedInBid.toLowerCase() !== 'n' && interestedInBid.toLowerCase() !== 'y')
+          {
+              return Promise.resolve(buildValidationResult(false,
+                                                            'InterestedInBid',
+                                                            `:confused: Sorry I didn't get that.Let me know if you interested or not by clicking one of the buttons below`,
+                                                            'Express your interest by using given options',
+                                                            'Click one of the options to proceed accordingly',true));
+          }
+      }
       if(dealerRef !== null)
       {
             return databaseManager.validateBidRefAndDealerRef(bidRef,dealerRef).then(response => {
                 if (_.isEmpty(response)) {
-                  return Promise.resolve(buildValidationResult(false,'DealerRef', 
-                                                                `Could not find Bids against the Bid reference. Please try again and make sure you have caps lock off :smile:`,
+                  return Promise.resolve(buildValidationResult(false,'DealerRef',
+                                                                `:heavy_exclamation_mark: Could not find Bids against the Bid reference. Please try again and make sure you have caps lock off :smile:`,
                                                                 null,null,null));
-                }  
+                }
                 var filenames='';
                 var isDealersBidExist;
                 response.Items.forEach(function(item) {
@@ -46,16 +59,16 @@ module.exports = function (bidRef,dealerRef,userId) {
                 });
                 if(!isDealersBidExist)
                 {
-                     return Promise.resolve(buildValidationResult(false,'DealerRef', 
-                                                                `Please check the Dearler Ref number as I don't see any bids from *${dealerRef}* for Bid Reference ${bidRef}`,
-                                                                null,null,null));     
+                     return Promise.resolve(buildValidationResult(false,'DealerRef',
+                                                                `:heavy_exclamation_mark: Check the Dealer Reference Number *${dealerRef}* as I couldn't find any bid from this dealear for ${bidRef} \r\n Enter the valid Dealer Reference Number from above list and try again`,
+                                                                null,null,null));
                 }
-                return Promise.resolve(buildValidationResult(true, null,null,null,null,null));   
-            });  
+                return Promise.resolve(buildValidationResult(true, null,null,null,null,null));
+            });
       }
-      return Promise.resolve(buildValidationResult(true,null,null,null,null,null));           
+      return Promise.resolve(buildValidationResult(true,null,null,null,null,null));
 };
-function buildValidationResult(isValid, violatedSlot, messageContent, 
+function buildValidationResult(isValid, violatedSlot, messageContent,
                                 responseCardTitle, responseCarSubtitle,isResponseCardRequired)
 {
   if (messageContent === null) {
